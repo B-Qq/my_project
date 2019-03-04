08fa7742f71367905c90779e2eabe71c5b48c437
bq5773718@163.com <q5983901>
2019-03-04 22:46:50 +0800

    newss

--- my_project/my_test.cpp
+++ my_project/my_test.cpp
@@ -5,3 +5,23 @@ my_test::my_test(const my_test &it)
 	
 }
 
+void my_test::setName(std::string & name)
+{
+	this->name = name;
+}
+
+void my_test::setAge(int age)
+{
+	this->age = age;
+}
+
+std::string my_test::getName() const
+{
+	return name;
+}
+
+int my_test::getAge() const
+{
+	return age;
+}
+
--- my_project/my_test.h
+++ my_project/my_test.h
@@ -9,10 +9,10 @@ public:
 	my_test() = default;
 	my_test(const my_test &it);
 	~my_test() = default;
-	//void setName(std::string &name);
-	//void setAge(int age);
-	//std::string getName() const;
-	//int getAge() const;
+	void setName(std::string &name);
+	void setAge(int age);
+	std::string getName() const;
+	int getAge() const;
 
 private:
 	std::string name;
